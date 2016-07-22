61e54d89ec1da1cc8a11df1c1a368c2047723a1e
Gerrard Geldenhuis <gerhardus.geldenhuis@gmail.com>
2016-07-22 13:24:19 +0100

    Hotfix1

--- checkversion.sh
+++ checkversion.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # For some reason this does not work directly as the conf_version script
 # Expecting the environment as the first parameter
+# Adding some random comment for testing
 cd /etc/puppetlabs/code/environments/$1
 /usr/bin/git status | /usr/bin/head -n1 | /usr/bin/cut -d" " -f4
